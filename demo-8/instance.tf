413f60d3089ac63ae4f8b6e0f5f11d328b92d69f
ahmed.basha <ahmed.basha@flixbus.com>
2023-07-20 00:04:33 +0200

    add demo-8 for creating ec2 instance in VPC

---
+++ demo-8/instance.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "example" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = var.INSTANCE_TYPE
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-public-1.id
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.mykeypair.key_name
+
+  # save private ip in a txt file
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.private_ip} >> private_ips.txt"
+  }
+}
+
+output "ip" {
+  value = aws_instance.example.public_ip
+}
